634658349f7171aee69e1826200ddf5454220d86
Eduard_Kulyk <kulikeduard89@gmail.com>
2023-02-20 23:17:37 +0100

    trying to add urls

--- utm_links/generator/admin.py
+++ utm_links/generator/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import UtmLink
+from .models import ShortIO
+
+admin.site.register(UtmLink)
+admin.site.register(ShortIO)
